5cc8000cc82e2d0121865eb075e6ebf45a4bed58
Bushra Hossain <bushrahossainonika@gmail.com>
2025-04-04 12:31:13 +0600

    telepoth

---
+++ Assets/_src/Scripts/Movements/Runtime/Datas/TelepothComponentDataAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TelepothComponentDataAuthoring : MonoBehaviour
+{
+    public float3 Position;
+
+    public class TelepothComponentDataBaker : Baker<TelepothComponentDataAuthoring>
+    {
+        public override void Bake(TelepothComponentDataAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new TelepothComponentData { Position = authoring.Position });
+        }
+    }
+}
---
+++ Assets/_src/Scripts/Movements/Runtime/Systems/TelepothSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Unity.Burst;
+using Unity.Entities;
+
+namespace _src.Scripts.Movements.Runtime.Systems
+{
+    public partial struct TelepothSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var telepothJobEntity = new TelepothJobEntity();
+            telepothJobEntity.ScheduleParallel();
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
+    }
+}
